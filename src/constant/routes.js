a5cf767649bdf6cf0dec593e0a3f281cae98d51d
FullStackDev <afullstackdev77@gmail.com>
2020-01-27 19:49:51 +0530

    Initial Structure

---
+++ src/constant/routes.js
@@ -0,0 +1,5 @@
+export const HomePage = '/home';
+export const LoginPage = '/login';
+export const Logoutpage = '/logout';
+export const ProfilePage = '/profile';
+export const SignUpPage = '/signup';
