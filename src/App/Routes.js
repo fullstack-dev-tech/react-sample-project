f970700e6a1d36aa630d6071f5a943e0f550633a
FullStackDev <afullstackdev77@gmail.com>
2020-01-27 21:01:48 +0530

    Header and Footer

--- src/App/Routes.js
+++ src/App/Routes.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-import HomePage from '../containers/Home'
+import HomePage from '../containers/Home';
+import LoginPage from '../containers/Login'
 
 const createRoutes = () => <Router>
     <Route path="/home" exact component={HomePage} />
+    <Route path="/login" exact component={LoginPage} />
     <Redirect from="/*" to="/home" />
 </Router>
 
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
 
 const HomePage = () => {
 
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <Header />
+      <Footer />
     </div>
   )
 }
