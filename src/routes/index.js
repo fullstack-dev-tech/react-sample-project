eb5f653a7b2d2b8742837e78df61beff2a8271c4
FullStackDev <afullstackdev77@gmail.com>
2020-01-31 17:59:30 +0530

    Added logic to redirect from profile to login, if not logged in

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import HomePage from '../containers/Home';
 import LoginPage from '../containers/Login';
 import SignUpPage from '../containers/Signup';
 import ProfilePage from '../containers/Profile';
+import { isAuthenticated } from '../firebase';
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        isAuthenticated() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
 
 const routes = (
   <Router>
-    <Route path="/" exact component={HomePage} />
-    <Route path="/login" exact component={LoginPage} />
-    <Route path="/signup" component={SignUpPage} />
-    <Route path="/profile" component={ProfilePage} />
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/login" exact component={LoginPage} />
+      <Route path="/signup" exact component={SignUpPage} />
+      <PrivateRoute path="/profile" exact component={ProfilePage} />
+      <Redirect from="/*" to="/" />
+    </Switch>
   </Router>
 );
 
