49781ab511788755bf65f837f3cd2127c2cda61b
FullStackDev <afullstackdev77@gmail.com>
2020-01-31 18:38:49 +0530

    Add method to get security questions

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -34,3 +34,16 @@ export const updateUser = async (email, data) => {
   const userRef = db.collection('users').doc(`${email}`);
   userRef.update({ ...data });
 }
+
+export const getSecurityQuestions = async () => {
+  let questions = []
+  const questionsRef = db.collection('securityQuestions');
+  const snapshots = await questionsRef.get();
+  if (snapshots.empty) {
+    throw new Error("No security questions found")
+  }
+  snapshots.forEach(doc => {
+    questions[doc.id] = doc.data().description;
+  });
+  return questions
+} 
