f59e53e077a6f8c617cdce5e10c0e63760c54b0f
FullStackDev <afullstackdev77@gmail.com>
2020-01-31 17:59:53 +0530

    combine reducers

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,9 +1,8 @@
-import AuthReducer from '../containers/Login/reducer';
-import ProfileRecucer from '../containers/Profile/reducer';
+import { combineReducers } from 'redux';
+import login from '../containers/Login/reducer';
+import user from '../containers/Profile/reducer';
+import signup from '../containers/Signup/reducer';
 
-const rootReducer = {
-  AuthReducer: AuthReducer.reducer,
-  ProfileReducer: ProfileRecucer.reducer
-}
+const authentication = combineReducers({ login, signup });
 
-export default rootReducer;
+export default combineReducers({ authentication, user });
