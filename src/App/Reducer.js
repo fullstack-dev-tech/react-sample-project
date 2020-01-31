85fa87e93d8e43b28885d930d5a3affc0285e827
FullStackDev <afullstackdev77@gmail.com>
2020-01-31 10:16:37 +0530

    Removed files

--- src/App/Reducer.js
+++
@@ -1,7 +0,0 @@
-import AuthReducer from '../containers/Login/reducer';
-import ProfileRecucer from '../containers/Profile/reducer';
-
-export const rootReducer = {
-  AuthReducer: AuthReducer.reducer,
-  ProfileReducer: ProfileRecucer.reducer
-}
